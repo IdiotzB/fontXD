36a515fade6e09e18c1674b92214ffa08f4cb884
IdiotzB <69295853+IdiotzB@users.noreply.github.com>
2020-09-02 20:21:37 +0700

    Add files via upload

---
+++ font.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Sengaja gk gw compile:)
+# GAK PENTING JUGA YEKANN:V
+# https://github.com/IdiotzB
+sleep 2.0
+echo "\33[0;32m▄█ █  █▀ █▄▄▄▄ ██   
+██ █▄█   █  ▄▀ █ █  
+██ █▀▄   █▀▀▌  █▄▄█ 
+▐█ █  █  █  █  █  █ 
+ ▐   █     █      █ 
+    ▀     ▀      █  
+                ▀   "
+echo "\33[33;1m Selamat Datang"
+echo "\33[31;1m-------------------------"
+echo " \33[33;1m(1).\33[33;1mLangsung Ke Fontnya?"
+echo " (2).\33[33;1mNgopi Dulu Boss?:v"
+echo " (3).\33[33;1mLogOut"
+echo "\33[37;1m-------------------------"
+read -p " Pilih • " pilih;
+if [ $pilih = "1" ]
+then
+    echo " \33[0;36mFONT XD NO BANDING"
+sleep 1
+    echo "\33[31;1m--------------------------"
+    echo " \33[1;33mPakai Bahasa Indo Aja :v "
+    echo " \33[1;33mPake Server Jepang"
+    echo " \33[1;33mNama Depan : 1 nama aja boss:v"
+sleep 1
+    echo " \33[1;33mNama Tengah : 〆"
+sleep 1
+    echo " \33[1;33mNama Belakang : ΧD."
+    echo "\33[37;1m--------------------------"
+#kasih batas
+elif [ $pilih = "2" ]
+then
+    echo " Sebutkan Nama Kopi?"
+read -p " Kopi apa? : " isi;
+sleep 1
+read -p " Rokok nya boss? : " isi;
+echo " \33[0;36mThanks Boss:v "
+sleep 1
+echo " \33[30;1mMoga aja sehat Selalu:)"
+#kasih lewat bang jago
+elif [ $pilih = "3" ]
+then
+     echo " GoodBy Thanks For Visit:)"
+exit
+fi
+
